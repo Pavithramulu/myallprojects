5b05151b0423079b5180039d2062383447cdb6a4
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-10 12:04:18 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'file.dart';
-
 void main() {
-  runApp(const  hello());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(500, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(500, name: TABLET),
+            const ResponsiveBreakpoint.resize(500, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: const homee(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
