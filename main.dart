9e69c97bc7d1a5caf1812de97cb04f74236b4cf1
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-10 12:10:14 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
-import 'demoeight.dart';
-import 'login.dart';
+
+import 'navigator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,29 +11,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context,child)=> ResponsiveWrapper.builder(child,
+      maxWidth : 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(500,name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(500,name: TABLET),
+          const ResponsiveBreakpoint.resize(500,name: DESKTOP),
+        ],
+        background: Container(
+          color: Color(0xFFF5F5F5),
+        ),
+       ),
+      initialRoute: "/",
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Wednesday(),
+      home: const Testone(),
     );
   }
 }
-
-
 
 
