e1a02f746c4a1c7a656ef39a18ebb4e0d15c0eb4
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-10 11:54:55 +0530

    Add files via upload

---
+++ 3.dart
@@ -0,0 +1,18 @@
+class Generated {
+  String? message;
+  String? status;
+
+  Generated({this.message, this.status});
+
+  Generated.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['status'] = this.status;
+    return data;
+  }
+}
