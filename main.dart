844baeb75752be3d0851ef86675683d0c9ca88a9
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-10 12:08:02 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 
-import 'exam.dart';
-import 'example1.dart';
-import 'list.dart';
-import 'num1.dart';
-import 'nums.dart';
+import 'calci.dart';
+import 'navigator.dart';
+import 'oddeven.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,18 +17,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(500, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(500, name: TABLET),
+            const ResponsiveBreakpoint.resize(500, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
-
       ),
-      home: Exam11(),
-      //Sejfdh(),
-     // Numbers1(),
-       //CalculateAge(),
+      home:  //Numbers(),
+      Numbers(),
     );
   }
 }
-
